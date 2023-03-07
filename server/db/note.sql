0b3047bbe50424a246e0b4be94a57ac2e4d543ac
shojib deb <shodeb123@gmail.com>
2023-03-07 11:21:14 +0000

    new commit

--- server/db/note.sql
+++ server/db/note.sql
@@ -1,11 +1,11 @@
 CREATE TABLE notes (
     note_id SERIAL NOT NULL UNIQUE,
+    PRIMARY KEY (note_id),
     note_title VARCHAR(255) NOT NULL,
     note_content VARCHAR(500) NOT NULL,
     note_category VARCHAR(255) NOT NULL,
     isPublic BOOLEAN DEFAULT FALSE NOT NULL,
     user_id SERIAL NOT NULL,
     img_url VARCHAR(500),
-    PRIMARY KEY (note_id),
     CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
