824e93edce4bd7a1c5e773ba53137ff5b1ecf514
Charlie <c.thurlow.ct@googlemail.com>
2023-03-09 11:24:56 +0000

    add controller-user test

--- server/__tests__/controller-user.test.js
+++ server/__tests__/controller-user.test.js
@@ -1,3 +1,23 @@
 require('dotenv').config();
-const { registerUser, loginUser, findById } = require('../controller/user.js')
+const { registerUser, loginUser, findById } = require('../controller/user.js');
 
+describe("resgisterUser", () => {
+    it("is a function", () => {
+        expect(registerUser).toBeDefined();
+        expect(registerUser instanceof Function).toEqual(true);
+    });
+});
+
+describe("loginUser", () => {
+    it("is a function", () => {
+        expect(loginUser).toBeDefined();
+        expect(loginUser instanceof Function).toEqual(true);
+    });
+});
+
+describe("findById", () => {
+    it("is a function", () => {
+        expect(findById).toBeDefined();
+        expect(findById instanceof Function).toEqual(true);
+    });
+});
