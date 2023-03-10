0eb3eefd572bf4c774d9621f4ff79328fd885bac
Shorizon <69736250+Shorizon@users.noreply.github.com>
2023-03-10 11:37:51 +0000

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base:"/Lap-3-Project-client/",
-  plugins: [react()],
-})
