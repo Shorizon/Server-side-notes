e1254e04760f0dea6f756c708db1b9c4e3ead7d6
Shorizon <69736250+Shorizon@users.noreply.github.com>
2023-03-10 11:37:26 +0000

    Delete src directory

--- src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Home } from './Home';
-export { default as Create } from './Create';
-export { default as Forum } from './Forum';
-export { default as PublicNote } from './PublicNote';
